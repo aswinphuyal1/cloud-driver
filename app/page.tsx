603b39c8bb1c62284f6053106b671b1062d797fc
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-09 19:14:53 +0545

    removed  all

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div >
-      <h1 className="text-3xl text-amber-200">the only cloud solution you nedd</h1>
-    </div>
-  );
-}
