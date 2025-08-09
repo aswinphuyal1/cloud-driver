007dcf98170e764461f9e060466d3d874dd9b771
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-09 21:26:54 +0545

    18.02

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-brand'>Home</div>
+    <div className="flex-center h-screen">
+        <h1 className='h1'>HI this is home page</h1>
+    </div>
+
   )
 }
 
