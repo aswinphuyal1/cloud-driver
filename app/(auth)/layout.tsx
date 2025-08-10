1b71db316df243dc161b147f01a0ded71cfaa4d3
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-10 21:12:28 +0545

    33.45

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -40,8 +40,18 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             className="h-auto w-[200px] lg:w-[250px]"
           />
         </div>
-
-        {children}
+        <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+          <div className="mb-16 lg:hidden">
+            <Image
+              src="/assets/icons/logo-full-brand.svg"
+              alt="logo"
+              width={224}
+              height={82}
+              className="h-auto w-[200px] lg:w-[250px] "
+            />
+          </div>
+          {children} 
+        </section>
       </section>
     </div>
   );
