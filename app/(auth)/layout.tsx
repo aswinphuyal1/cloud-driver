97ac390a0109eae4c52a8445f3eec829813b81b0
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-11 19:29:06 +0545

    ui added dummy

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -11,7 +11,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             alt="logo"
             width={224}
             height={82}
-            className="h-auto"
+            className="h-auto "
           />
 
           <div className="space-y-5 text-white">
@@ -30,16 +30,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         </div>
       </section>
 
-      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
-        <div className="mb-16 lg:hidden">
-          <Image
-            src="/assets/icons/logo-full-brand.svg"
-            alt="logo"
-            width={224}
-            height={82}
-            className="h-auto w-[200px] lg:w-[250px]"
-          />
-        </div>
         <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
           <div className="mb-16 lg:hidden">
             <Image
@@ -52,7 +42,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           </div>
           {children} 
         </section>
-      </section>
+  
     </div>
   );
 };
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
-type fromtype='sign-in'|"sign-up"
-const AuthForm = ({type}:{type:'fromtype'}) => {
-  return (
-    <div>AuthForm</div>
-  )
-}
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
-export default AuthForm
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+
+type fromtype = "sign-in" | "sign-up";
+const AuthForm = ({ type }: { type: fromtype }) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  const onSubmit= async(values: z.infer<typeof formSchema>)=> {
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+      {/* opt verification */}
+    </>
+  );
+};
+
+export default AuthForm;
