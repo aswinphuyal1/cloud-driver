7ae5c626e65e662397f05138fd3a5406a029d9de
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-13 01:24:29 +0545

    for creatind users

---
+++ lib/appwrite/index.ts
@@ -0,0 +1,47 @@
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
+import { appwriteconfig } from "./config";
+import { cookies } from "next/headers";
+import { error } from "console";
+export const createsessionclient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteconfig.endpointurl)
+    .setProject(appwriteconfig.projectid);
+
+  const session = (await cookies()).get("appwrite-session");
+  if (!session || !session.value) {
+    throw new Error("no session");
+  } else {
+    client.setSession(session.value);
+  }
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+export const createadminclient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteconfig.endpointurl)
+    .setProject(appwriteconfig.projectid)
+    .setKey(appwriteconfig.secretkey);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
