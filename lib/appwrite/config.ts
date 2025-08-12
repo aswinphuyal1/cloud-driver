c8ef529ec26afc1be904d2c04045f51b945fc946
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-13 00:12:28 +0545

    taking varible form .env so it is easy to be use over an app

---
+++ lib/appwrite/config.ts
@@ -0,0 +1,9 @@
+export const appwriteconfig = {
+  endpointurl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+  projectid: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!,
+  databaseid: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
+usercollectionid: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
+filescollectionid:process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION!,
+bucketid:process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
+secretkey:process.env.NEXT_APPWRITE_KEY!
+};
