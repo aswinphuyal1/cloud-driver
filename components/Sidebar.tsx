17498497c95a7e63638d003d44936253347f5a01
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-13 18:55:09 +0545

    Navbar design done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,12 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import { navItems } from "@/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <aside className="sidebar">
       <Link href="/">
@@ -9,21 +14,43 @@ const Sidebar = () => {
           src="/assets/icons/logo-full-brand.svg"
           alt="logo"
           width={160}
-          height={160}
+          height={50}
           className="hidden h-auto lg:block"
         />
-        <Image src="/assets/icons/logo-brand.svg"
-        alt="logo"
-        width={52}
-        height={52}
-        className="lg:hidden" />
+        <Image
+          src="/assets/icons/logo-brand.svg"
+          alt="logo"
+          width={52}
+          height={52}
+          className="lg:hidden"
+        />
       </Link>
       <nav className="sidebar-nav">
         <ul className="flex flex-1 flex-col gap-6">
-          {}
-
+          {/* {[{name:home,path:'/'}]}.map((item.index))=>{} */}
+          {navItems.map(({ url, name, icon }) => (
+            <Link key={name} href={url} className="lg:w-full">
+              <li
+                className={cn(
+                  "sidebar-nav-item",
+                  pathname === url && "shad-active"
+                )}
+              >
+                <Image
+                  src={icon}
+                  alt={name}
+                  width={24}
+                  height={24}
+                  className={cn(
+                    "nav-icon",
+                    pathname === url && "nav-icon-active"
+                  )}
+                />
+                <p className="hidden lg:block">{name}</p>
+              </li>
+            </Link>
+          ))}
         </ul>
-
       </nav>
     </aside>
   );
