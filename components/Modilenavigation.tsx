428b346210f9ef7d2969fa41ce54fabb4b8b3364
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-16 23:47:46 +0545

    modilenavigation done but logot function and fileuploader function not completed

--- components/Modilenavigation.tsx
+++ components/Modilenavigation.tsx
@@ -1,26 +1,35 @@
-"use client"
-import React from 'react'
-import Image from 'next/image';
-import { useState } from 'react';
+"use client";
+import React from "react";
+import Image from "next/image";
+import { useState } from "react";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { string } from 'zod';
-interface Props
-{
-  ownerId:string
-  ,accountId:string
-  ,fullName:string
-  ,avatar:string
-  ,email:string
+import { Separator } from "@/components/ui/separator";
+import { navItems } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+interface Props {
+  fullName: string;
+  avatar: string;
+  email: string;
+  ownerId:string,
+  accountId:string
 }
-const Modilenavigation = ({ownerId,accountId,fullName,avatar,email}:Props) => {
-  const [open, setopen] = useState(false)
+const Modilenavigation = ({
+  ownerId,
+  accountId,
+  fullName,
+  avatar,
+  email,
+}: Props) => {
+  const [open, setopen] = useState(false);
+   const pathname = usePathname();
   return (
     <header className="mobile-header">
       <Image
@@ -32,10 +41,15 @@ const Modilenavigation = ({ownerId,accountId,fullName,avatar,email}:Props) => {
       />
       <Sheet open={open} onOpenChange={setopen}>
         <SheetTrigger>
-          <Image src="/assets/icons/menu.svg" alt="" width={30} height={30} />
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="Open navigation menu"
+            width={30}
+            height={30}
+          />
         </SheetTrigger>
         <SheetContent className="shad-sheet h-screen px-3">
-          <SheetTitle>
+          <SheetHeader>
             <div className="header-user">
               <Image
                 src={avatar}
@@ -44,24 +58,63 @@ const Modilenavigation = ({ownerId,accountId,fullName,avatar,email}:Props) => {
                 alt="avatar"
                 className="header-user-avatar"
               />
-              <div className='sm:hidden lg:block' >
-<p className='subtitle-2 capitalize'>{fullName}</p>
-<p className='caption'>{email}</p>
-
-
+              <div className="lg:hidden">
+                <p className="subtitle-2 capitalize">{fullName}</p>
+                <p className="caption">{email}</p>
               </div>
             </div>
-          </SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
+          </SheetHeader>
+          <Separator className="my-4" />
+          <nav className="mobile-nav">
+            <ul className="mobile-nav-list">
+              {navItems.map(({ url, name, icon }) => (
+                <Link key={name} href={url} className="lg:w-full">
+                  <li
+                    className={cn(
+                      "mobile-nav-item",
+                      pathname === url && "shad-active"
+                    )}
+                  >
+                    <Image
+                      src={icon}
+                      alt={name}
+                      width={24}
+                      height={24}
+                      className={cn(
+                        "nav-icon",
+                        pathname === url && "nav-icon-active"
+                      )}
+                    />
+                    <p className="text-base font-medium">{name}</p>
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+          <Separator className="my-5 bg-light-200/20" />
+          <div className="flex flex-col justify-between gap-5 pb-5">
+            Fileuploader
+          </div>
+          <form>
+            <Button type="submit" className="mobile-sign-out-button" onClick={()=>
+              {
+
+              }
+            }>
+              <Image
+                src="/assets/icons/logout.svg"
+                alt="logo"
+                width={24}
+                height={24}
+                
+              />
+              <p>Logout</p>
+            </Button>
+          </form>
         </SheetContent>
       </Sheet>
     </header>
   );
-}
+};
 
-export default Modilenavigation
-
-
+export default Modilenavigation;
