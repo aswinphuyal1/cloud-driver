9002e55590a5277112026fed4ef8bf1dce4f82e9
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-16 16:30:01 +0545

    2.8

--- components/Modilenavigation.tsx
+++ components/Modilenavigation.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import Image from 'next/image';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 const Modilenavigation = () => {
   return (
-    <div>Modilenavigation</div>
+   <header>
+    <Image src="/assets/icons/logo-full-brand.svg" alt='logo'
+    width={120} height={52} />
+   </header>
   )
 }
 
