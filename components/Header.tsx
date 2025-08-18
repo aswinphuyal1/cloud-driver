a0a616b5856d1a8ee01b479f84dc7fcb15491a09
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-18 19:23:32 +0545

    signout function in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,15 +3,27 @@ import { Button } from './ui/button'
 import Image from 'next/image'
 import Search from './Search';
 import Fileuploader from './Fileuploader';
+import { Signoutuser } from '@/lib/actions/user.actions';
 const Header = () => {
   return (
     <header className="header">
-      <Search/>
+      <Search />
       <div className="header-wrapper">
-        <Fileuploader/>
-        <form >
+        <Fileuploader />
+        <form
+          action={async () => {
+            "use server";
+            await Signoutuser()
+          }}
+        >
           <Button type="submit" className="sign-out-button">
-            <Image src="/assets/icons/logout.svg" alt="logo" width={24} height={24} className='w-6' />
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
           </Button>
         </form>
       </div>
