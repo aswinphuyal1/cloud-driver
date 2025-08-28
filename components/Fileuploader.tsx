9d208e4f8c908cf4b58314e6cc5b9095f7010b1a
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-28 20:43:45 +0545

    npm i react-dropzone Simple React hook to create a HTML5-compliant drag'n'drop zone for files.

--- components/Fileuploader.tsx
+++ components/Fileuploader.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
-const Fileuploader = () => {
-  return (
-    <div>Fileuploader</div>
-  )
+"use client";
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+interface Props {
+  ownerid: string;
+  accountid: string;
+  className: string;
 }
+const Fileuploader = ({ ownerid, accountid, className }: Props) => {
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
-export default Fileuploader
+  return (
+    <div {...getRootProps()} className="cursor-pointer">
+      <input {...getInputProps()} />
+    
+      {isDragActive ? (
+        <p>Upload</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
+};
+
+export default Fileuploader;
