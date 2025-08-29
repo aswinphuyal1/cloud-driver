3e86c06bb1690d6f47a3cd12fa166ff2c8901a85
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-29 21:24:08 +0545

    2.52

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -1,8 +1,48 @@
-"use server"
-
+"use server";
+import { InputFile } from "node-appwrite/file";
+import { createadminclient } from "../appwrite";
+import { appwriteconfig } from "../appwrite/config";
+import { ID } from "node-appwrite";
+import { constructFileUrl, getFileType } from "../utils";
+import { use } from "react";
+const handleError = (error: unknown, message: string) => {
+  console.error(message, error);
+  throw new Error(message);
+};
 export const uploadfilr = async ({
   file,
   ownerId,
   accountid,
   path,
-}: UploadFileProps) => {};
+}: UploadFileProps) => {
+  const { storage, databases } = await createadminclient();
+  try {
+    const inputfile = InputFile.fromBuffer(file, file.name);
+
+    const bucketfile = await storage.createFile(
+    
+    //where we going to store
+        appwriteconfig.bucketid,
+        //input garne ko id
+      ID.unique(),
+      //kuninput garne
+      inputfile
+    );
+
+    const filedocument=
+    {
+        type:getFileType(bucketfile.name).type,
+        name:bucketfile.name,
+        url:constructFileUrl(bucketfile.$id),
+        extension:getFileType(bucketfile.name).extension,
+        size:bucketfile.sizeOriginal,
+        owner:ownerId,
+        accountid,
+        users:[],
+        bucketfileID:bucketfile.$id
+    }
+
+  } catch (error) {
+    handleError(error, "file upload fail!!!!!!");
+  }
+};
