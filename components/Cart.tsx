96978f07fe2a4d51525ffcbb5302efec57cb96bb
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-30 20:56:53 +0545

    new Formatteddatetime component created

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -2,20 +2,33 @@ import Link from 'next/link'
 import { Models } from 'node-appwrite'
 import React from 'react'
 import Thumbnail from './Thumbnail'
+import { convertFileSize } from '@/lib/utils'
+import Formatteddatetime from './Formatteddatetime'
 
 const Cart = ({file}:{file:Models.Document}) => {
   return (
-    <Link href={file.url} target='_blank' className='file-card'>
-        <div className='flex justify-between'>
-<Thumbnail name={file.name} type={file.type} extension={file.extensaion} url={file.url} className='!size-20'imageClassName='!size-11'/>
-<div>
-    
-</div>
+    <Link href={file.url} target="_blank" className="file-card">
+      <div className="flex justify-between">
+        <Thumbnail
+          name={file.name}
+          type={file.type}
+          extension={file.extensaion}
+          url={file.url}
+          className="!size-20"
+          imageClassName="!size-11"
+        />
+        <div className="flex flex-col items-end justify-between">
+          //action drop down
+          <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
-{file.name}
-    
+      </div>
+      <div className="file-card-details">
+        
+        <p className="subtitle-2 line-clamp-1">{file.name}</p>
+        <Formatteddatetime/>
+      </div>
     </Link>
-  )
+  );
 }
 
 export default Cart
---
+++ components/Formatteddatetime.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Formatteddatetime = () => {
+  return (
+    <div>Formatteddatetime</div>
+  )
+}
+
+export default Formatteddatetime
