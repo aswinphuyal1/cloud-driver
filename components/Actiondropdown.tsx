4c3dd611a462853bdb1ca3f8a2b318952a2cd203
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-08-30 23:58:50 +0545

    npx shadcn@latest add dialog from shadcn

--- components/Actiondropdown.tsx
+++ components/Actiondropdown.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 const Actiondropdown = () => {
   return (
-    <div>Actiondropdown</div>
-  )
+    <Dialog>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 }
 
 export default Actiondropdown
