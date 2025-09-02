ac71d4de08b67f56f5c4d53d2f966addd9ffaee6
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-09-02 17:41:17 +0545

    Debounce function work fine

--- components/Search.tsx
+++ components/Search.tsx
@@ -8,49 +8,44 @@ import { getfiles } from "@/lib/actions/file.actions";
 import { Models } from "node-appwrite";
 import Thumbnail from "./Thumbnail";
 import Formatteddatetime from "./Formatteddatetime";
-
+import { useDebounce } from "use-debounce";
 
 const Search = () => {
   const [query, setquery] = useState("");
   const searchParams = useSearchParams();
   const searchquery = searchParams.get("query") || "";
-
+  const [debouncequery] = useDebounce(query, 2000);
   const [result, setresult] = useState<Models.Document[]>([]);
   const [open, setopen] = useState(false);
-const router=useRouter();
-const path=usePathname()
+  const router = useRouter();
+  const path = usePathname();
   useEffect(() => {
     const fetchfiles = async () => {
-      if(!query)
-      {
-        setresult([])
-        setopen(false)
-        return router.push(path.replace(searchParams.toString(),''));
-
+      if (debouncequery.length === 0) {
+        setresult([]);
+        setopen(false);
+        return router.push(path.replace(searchParams.toString(), ""));
       }
-     const files = await getfiles({ types: [], searchText: query});
+      const files = await getfiles({ types: [], searchText: debouncequery });
       setresult(files.documents);
       setopen(true);
     };
     fetchfiles();
-  }, [query]);
+  }, [debouncequery]);
   useEffect(() => {
     if (!searchquery) {
       setquery("");
     }
   }, [searchquery]);
 
-
-  const handelclickitems =
-  (file:Models.Document)=>
-  {
-    setopen(false)
-    setresult([])
+  const handelclickitems = (file: Models.Document) => {
+    setopen(false);
+    setresult([]);
 
     router.push(
-      `/${(file.type === "video" || file.type === "audio" )? "media" : file.type + "s"}?query=${query}`
+      `/${file.type === "video" || file.type === "audio" ? "media" : file.type + "s"}?query=${query}`
     );
-  }
+  };
   return (
     <div className="search">
       <div className="search-input-wrapper">
@@ -74,7 +69,7 @@ const path=usePathname()
                 <li
                   className="flex items-center justify-between"
                   key={file.$id}
-                  onClick={()=>handelclickitems(file)}
+                  onClick={() => handelclickitems(file)}
                 >
                   <div className="flex cursor-pointer items-center gap-4 min-w-0">
                     <Thumbnail
