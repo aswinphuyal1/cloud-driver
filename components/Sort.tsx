8da520a89f4a6adfdb6c698175e06ef335ce3bdc
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-09-02 18:16:35 +0545

    sort function work perfect

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,9 +1,37 @@
+"use client"
 import React from 'react'
-
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { usePathname, useRouter } from 'next/navigation';
+import { sortTypes } from '@/constants';
 const Sort = () => {
+  const path =usePathname()
+  const router =useRouter()
+  const handelsort=(value:string)=>
+  {
+    router.push(`${path}?sort=${value}`)
+  }
   return (
-    <div>Sort</div>
-  )
+    <Select onValueChange={handelsort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-slect-content">
+        {sortTypes.map((sort) => (
+          <SelectItem  key={sort.label} value={sort.value}>
+            {sort.label}
+          </SelectItem>
+        ))}
+
+       
+      </SelectContent>
+    </Select>
+  );
 }
 
 export default Sort
